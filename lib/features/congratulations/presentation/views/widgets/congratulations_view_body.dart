f4443cb38647121b250eab3fb25e501b88af5611
ibrahim Osama <zx.hema72@gmail.com>
2023-12-04 23:52:45 +0200

    register done

--- lib/features/congratulations/presentation/views/widgets/congratulations_view_body.dart
+++ lib/features/congratulations/presentation/views/widgets/congratulations_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foody/constants.dart';
 import 'package:foody/core/utils/asset_data.dart';
+import 'package:foody/core/utils/styles.dart';
 
 class CongratulationsViewBody extends StatelessWidget {
   const CongratulationsViewBody({super.key});
@@ -7,13 +9,29 @@ class CongratulationsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetData.congrats),
-        SizedBox(
+        Image.asset(
+          AssetData.congrats,
+          height: 160,
+        ),
+        const SizedBox(
           height: 30,
         ),
-        Text('Congratulations !'),
-        Text('Your Profile Is Ready To Use'),
+        Text(
+          'Congrats!',
+          textAlign: TextAlign.center,
+          style: Styles.textStyle30.copyWith(color: kPrimaryColor),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'Your Profile Is Ready To Use',
+          textAlign: TextAlign.center,
+          style: Styles.textStyle22,
+        ),
       ],
     );
   }
