be255d643e019da34b32921d84fa927e762727c4
ibrahim Osama <zx.hema72@gmail.com>
2023-12-08 01:44:42 +0200

    UI done

--- lib/features/cart/presentation/views/widgets/cart_view_body.dart
+++ lib/features/cart/presentation/views/widgets/cart_view_body.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:foody/constants.dart';
 import 'package:foody/core/utils/styles.dart';
 import 'package:foody/features/cart/presentation/manager/cart_cubit/cart_cubit.dart';
+import 'package:foody/features/cart/presentation/views/widgets/product_container.dart';
 
 class CartViewBody extends StatefulWidget {
   const CartViewBody({super.key, required this.email});
@@ -35,7 +38,23 @@ class _CartViewBodyState extends State<CartViewBody> {
                   child: AnimatedList(
                     key: _listKey,
                     initialItemCount: state.cartItems.length,
-                    itemBuilder: (context, index, animation) => Container(),
+                    itemBuilder: (context, index, animation) => Slidable(
+                      startActionPane:
+                          ActionPane(motion: StretchMotion(), children: [
+                        SlidableAction(
+                          onPressed: (context) {},
+                          icon: Icons.delete,
+                          label: 'Delete',
+                          backgroundColor: kPrimaryColor,
+                          padding: EdgeInsets.all(10),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ]),
+                      child: ProductContainer(
+                        index: index,
+                        state: state,
+                      ),
+                    ),
                   ),
                 )
               ],
---
+++ lib/features/cart/presentation/views/widgets/product_container.dart
@@ -0,0 +1,98 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../constants.dart';
+import '../../../../../core/utils/styles.dart';
+import '../../manager/cart_cubit/cart_cubit.dart';
+
+class ProductContainer extends StatelessWidget {
+  const ProductContainer({
+    super.key,
+    required this.state,
+    required this.index,
+  });
+  final CartSuccess state;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border.all(width: 1, color: kPrimaryColor),
+          borderRadius: BorderRadius.circular(15)),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: CachedNetworkImage(
+                height: 100, imageUrl: state.cartItems[index].image),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Text(
+                  state.cartItems[index].name,
+                  style: Styles.textStyle22,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    state.cartItems[index].price.toString() + '\$',
+                    style: Styles.textStyle22.copyWith(
+                        color: kPrimaryColor, fontWeight: FontWeight.w900),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.10,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        margin: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.remove,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      Text(
+                        state.cartItems[index].quantity.toString(),
+                        style: Styles.textStyle22,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        margin: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
